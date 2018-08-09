02d0a6bbe83656998a893fadf6150897d2a3fbde
boss <r4b2c1@gmai.com>
2018-08-09 17:58:03 -0500

    update

--- keys.js
+++ keys.js
@@ -1,16 +1,20 @@
-console.log('this is loaded');
+console.log("Keys loaded");
+
 
 exports.twitter = {
-  consumer_key:zQnbFtNC6qCrgofiVrHX08PJE ,
-  consumer_secret:	RLXFcT4isMzlaYWVfUmkKjc0uxfd6SrI7lkQs86RrQIJFdk7Zo,
-  access_token_key:	1019656577236815872-ACsgALe7Qb1uhFHWsPODwM4BVxqFGT, 
-  access_token_secret: 	emSenW3W1KeJaJXEgkZBb2vjQ8Iib9lLrMUUSiPaBKT4W,
+  consumer_key: process.env.zQnbFtNC6qCrgofiVrHX08PJE,
+  consumer_secret: process.env.RLXFcT4isMzlaYWVfUmkKjc0uxfd6SrI7lkQs86RrQIJFdk7Zo,
+  access_token_key: process.env.1019656577236815872-ACsgALe7Qb1uhFHWsPODwM4BVxqFGT,
+  access_token_secret: process.env.emSenW3W1KeJaJXEgkZBb2vjQ8Iib9lLrMUUSiPaBKT4W
 };
+
+
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.9c9c3e85a098481693212473b587c5d0,
+  secret: process.env.8262dde598274951ade8222387964cd7
 };
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+
+
+
